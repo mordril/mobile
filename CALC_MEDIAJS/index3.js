7e6983e968f4f0f1bed388b9db16ec989086d994
henrique.nunes.ferreira <133805001+mordril@users.noreply.github.com>
2023-08-01 16:28:07 -0300

    Add files via upload

---
+++ CALC_MEDIAJS/index3.js
@@ -0,0 +1,6 @@
+function clicou() {
+    var media= document.getElementById('media')
+    var br1= document.getElementById ('barra1')
+    var br2= document.getElementById ('barra2')
+    var br1= Number(b1.value)
+    var br2= Number(b1.value)
---
+++ guiaturistico/guiaTuristico.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GuiaTuristico</title>
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+    <div class="topo">
+        <img src="img/araucaria.png" style="width: 200px;height: 200px;">   
+    </div>
+
+    <div class="cidade1">
+        <h3> Curitiba</h3>
+        <img src="" style="width: 3px;height: 3px;">
+        <img class="imagem" src="img/curitiba.jpg" style="width:200px;height:150px;">
+        <p>Curitiba é a capital do estado do Paraná, na região sul do Brasil. Conhecida como centro cultural, Curitiba abriga vários espaços para apresentações, como a Ópera de Arame, uma estrutura de aço tubular com telhado transparente, e o enorme Centro Cultural Teatro Guaíra, que apresenta uma programação variada.</p>
+    </div>
+
+    <div class="cidade2">
+        <h3> Ponta Grossa</h3>
+        <img src="" style="width: 3px;height: 3px;">
+        <img class="imagem" src="img/vila-velha.jpg" style="width:200px;height:150px;">
+        <p>Ponta Grossa é um município brasileiro do estado do Paraná, do qual é o quarto mais populoso, com 391 654 habitantes, conforme estimativa do IBGE publicada em 2023. Conta com a nona maior população do Sul do Brasil e a septuagésima sexta do país.Considerado o mais famoso atrativo turístico de Ponta Grossa, o Parque Estadual de Vila Velha possui três atrações: Arenitos, Furnas e Lagoa Dourada.
+        </p>
+    </div>
+
+    <div class="cidade3">
+        <h3> Foz do iguaçu</h3>
+        <img src="" style="width: 3px;height: 3px;">
+        <img class="imagem" src="img/foz do iguaçu.webp" style="width:200px;height:150px;">
+        <p>Foz do Iguaçu, cidade no estado brasileiro do Paraná, é a principal base para visitar as famosas Cataratas do Iguaçu, uma das maiores cachoeiras do mundo. Com uma extensão de 2,7 km e atravessando a fronteira com a Argentina, as cataratas são formadas por centenas de cascatas, entre elas a Garganta do Diabo, com 80 m de altura. Passeios em barcos infláveis durante os quais os turistas ficam encharcados são uma forma muito comum de apreciar esse espetáculo natural.</p>
+    </div>
+
+    <div class="cidade4">
+        <h3>Londrina</h3>
+        <img  style="width: 3px;height: 3px;">
+        <img class="imagem" src="img/londrina.webp" style="width:200px;height: 150px">
+        <p>Londrina é um município brasileiro localizado no estado do Paraná, na Região Sul do Brasil, distando 381 km da capital paranaense, Curitiba. A pequena cidade conta com 5 lindas praias para você conferir, sendo as principais a Praia do Leste e a Praia de Pontal do Sul.</p>
+    </div>
+    
+</body>
+</html>
